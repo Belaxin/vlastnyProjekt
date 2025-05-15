638d09200fab9849bdd7c93f9b994e5e03643c08
Tomas Matis <tomasbelaxinen@gmail.com>
2025-05-15 11:43:24 +0200

    -hodina End

--- index.php
+++ index.php
@@ -1,1 +1,59 @@
-//test Push
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel= "stylesheet" href="style.css"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+<section class ="main">
+    <header>
+        <section class= "headerContainer">
+            <img src="Logo.png" alt="logo">
+            <nav>
+                <a href="index.php" class = "active">Home</a>
+                <a href="#Location">Location</a>
+                <a href="#Store">Store</a>
+                <a href="#About" class = "Active">About</a>
+                <a href="Cart.php">Cart</a>
+            </nav>
+        </section>
+    </header>
+
+    <section class = "hero" id = "#About">
+        <section class = "heroText">
+            <h2>Oregon’s oldest nonprofit.</h2>
+            <p>
+                We present Catty, a safe haven for feline friends in need. Our mission is to rescue, care for, and rehome cats of all ages, breeds, 
+                and personalities. At Catty, we believe every cat deserves a loving home where they can thrive and feel cherished. Our dedicated team of 
+                volunteers works tirelessly to provide medical care, socialization, and a nurturing environment for each cat in our shelter. Whether 
+                you're looking to adopt a new furry companion or support our efforts, you're helping us make a difference in the lives of cats. Join us 
+                in giving these incredible animals a second chance at happiness!
+            </p>
+            <a href="#"  class = "whiteButton" >Learn more</a>
+        </section>
+        <div class = "imageHero"></div>
+    </section>
+
+    <section class="adoptions" id = "#">
+        <h1>Available cuties</h1>
+        <div>
+            <a href="#" class = "active">Portland</a>
+            <a href="#">Salem</a>
+            <a href="#">Hilsboro</a>
+        </div>
+        <section class ="beltView">
+            <section class  ="belt">
+                <section class="catCard">
+
+                </section>
+            </section>
+        </section>
+    </section>
+</section>
+</body>
+</html>
