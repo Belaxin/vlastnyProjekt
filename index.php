aefde838676964b6b1d5de819236313b6934e4e9
Tomas Matis <tomasbelaxinen@gmail.com>
2025-06-05 11:57:15 +0200

    -add shop and locations

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <img src="Logo.png" alt="logo">
             <nav>
                 <a href="index.php" class = "active">Home</a>
-                <a href="#Location">Location</a>
+                <a href="#Locations">Location</a>
                 <a href="#Store">Store</a>
                 <a href="#About" class = "Active">About</a>
                 <a href="Cart.php">Cart</a>
@@ -24,7 +24,7 @@
         </section>
     </header>
 
-    <section class = "hero" id = "#About">
+    <section class = "hero" id = "About">
         <section class = "heroText">
             <h2>Oregon’s oldest nonprofit.</h2>
             <p>
@@ -50,7 +50,7 @@
         <div class="show">
             <div class="belt">
             <div class="belt-item">
-                <img src="cat1.png" alt="">
+                <img src="cat1.png" alt="cat">
                 <div class="beltText">
                     <h2>MeiMei</h2>
                     <p>Our cutest specimen indeed, MeiMei also known as sister Mei comes from the distant land of China. We rescued her after one of our fans notified us of her. 
@@ -61,56 +61,56 @@
             </div>
 
             <div class="belt-item">
-            <img src="cat2.png" alt="">
+            <img src="cat2.png" alt="cat">
                 <div class="beltText">
-                    <h2>MeiMei</h2>
-                    <p>Our cutest specimen indeed, MeiMei also known as sister Mei comes from the distant land of China. We rescued her after one of our fans notified us of her. 
-                        Her previous owner was a well known animal abuser so we decided to take her into our hands. She has no teeth but that brings no problem as she is resilient and 
-                        strong.</p>
+                    <h2>Johnny</h2>
+                    <p>Johnny is a stray with a brown-gray coat that helps him blend into the shadows. His amber eyes are always alert, 
+                        observing everything with quiet curiosity. He moves gracefully, despite his rough fur, which shows signs of his life outdoors. 
+                        Johnny often lounges in the sun, enjoying the warmth and rest.</p>
                     <a href="#">Adopt</a>
                 </div>
             </div>
 
             <div class="belt-item">
-            <img src="cat3.png" alt="">
+            <img src="cat3.png" alt="cat">
                 <div class="beltText">
-                    <h2>MeiMei</h2>
-                    <p>Our cutest specimen indeed, MeiMei also known as sister Mei comes from the distant land of China. We rescued her after one of our fans notified us of her. 
-                        Her previous owner was a well known animal abuser so we decided to take her into our hands. She has no teeth but that brings no problem as she is resilient and 
-                        strong.</p>
+                    <h2>Cupcake</h2>
+                    <p>Cupcake is a sweet little cat with orange, white, and black fur that resembles swirls of frosting. Her playful yet gentle nature
+                         makes her as delightful as her name. She loves chasing ribbons and tilts her head curiously when spoken to, as if she 
+                         understands every word. The name suits her perfectly, capturing her charm and irresistible cuteness.</p>
                     <a href="#">Adopt</a>
                 </div>
             </div>
 
             <div class="belt-item">
-            <img src="cat4.png" alt="">
+            <img src="cat4.png" alt="cat">
                 <div class="beltText">
-                    <h2>MeiMei</h2>
-                    <p>Our cutest specimen indeed, MeiMei also known as sister Mei comes from the distant land of China. We rescued her after one of our fans notified us of her. 
-                        Her previous owner was a well known animal abuser so we decided to take her into our hands. She has no teeth but that brings no problem as she is resilient and 
-                        strong.</p>
+                    <h2>Tiny</h2>
+                    <p>Tiny is a small, brown-furred cat with a big personality that contrasts her name. Found on the street, she quickly showed she was
+                         anything but fragile, her resilience shining through. Her soft, warm coat matches her gentle yet adventurous spirit. Tiny’s name
+                          is a sweet nod to her petite size and the way she curled up so snugly the day she was rescued.</p>
                     <a href="#">Adopt</a>
                 </div>
             </div>
 
             <div class="belt-item">
-            <img src="cat5.png" alt="">
+            <img src="cat5.png" alt="cat">
                 <div class="beltText">
-                    <h2>MeiMei</h2>
-                    <p>Our cutest specimen indeed, MeiMei also known as sister Mei comes from the distant land of China. We rescued her after one of our fans notified us of her. 
-                        Her previous owner was a well known animal abuser so we decided to take her into our hands. She has no teeth but that brings no problem as she is resilient and 
-                        strong.</p>
+                    <h2>Skull</h2>
+                    <p>Skull is a rugged, cool cat with a name as bold as his appearance. Missing half an ear and one eye, he carries his scars like 
+                        badges of honor. His sleek movements and piercing gaze give him an air of mystery and strength. The name Skull fits perfectly, 
+                        capturing his fierce, unyielding spirit and unforgettable presence.</p>
                     <a href="#">Adopt</a>
                 </div>
             </div>
 
             <div class="belt-item">
-            <img src="cat6.png" alt="">
+            <img src="cat6.png" alt="cat">
                 <div class="beltText">
-                    <h2>MeiMei</h2>
-                    <p>Our cutest specimen indeed, MeiMei also known as sister Mei comes from the distant land of China. We rescued her after one of our fans notified us of her. 
-                        Her previous owner was a well known animal abuser so we decided to take her into our hands. She has no teeth but that brings no problem as she is resilient and 
-                        strong.</p>
+                    <h2>Fresh cat</h2>
+                    <p>Blade is a sleek gray cat who loves to hang out at the barber shop, where his name gets plenty of laughs. 
+                        His smooth fur almost looks like it’s been styled, and he sits proudly as if he’s waiting for a fresh fade. Blade’s calm, 
+                        confident demeanor and sharp appearance make his name a perfect match. He’s the coolest cat in the room, always stealing the show.</p>
                     <a href="#">Adopt</a>
                 </div>
             </div>
@@ -123,18 +123,120 @@
     </section>
 
 
-    <section class ="store">
+    <section class ="store" id = "Store">
         <h1>Store</h1>
         <div class = "products">
-            <div class="product-item">Item 1</div>
-            <div class="product-item">Item 2</div>
-            <div class="product-item">Item 3</div>
-            <div class="product-item">Item 4</div>
-            <div class="product-item">Item 5</div>
-            <div class="product-item">Item 6</div>
+            <div class="product-item">
+                <img src="product3.png" alt="clippers">
+                <div class="product-item-text">
+                    <h3>Tick-away spray</h3>
+                    <div class="product-item-text-sub">
+                        <h3>$10</h3>
+                        <a href="#"  class = "blackButton" >Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="product-item">
+                <img src="product2.png" alt="kibble">
+                <div class="product-item-text">
+                    <h3>Kibble 5kg</h3>
+                    <div class="product-item-text-sub">
+                        <h3>$20</h3>
+                        <a href="#"  class = "blackButton" >Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="product-item">
+                <img src="product1.png" alt="spray">
+                <div class="product-item-text">
+                    <h3>Nail clippers</h3>
+                    <div class="product-item-text-sub">
+                        <h3>$40</h3>
+                        <a href="#"  class = "blackButton" >Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="product-item">
+                <img src="product6.png" alt="litter">
+                <div class="product-item-text">
+                    <h3>Kibble bowl</h3>
+                    <div class="product-item-text-sub">
+                        <h3>$15</h3>
+                        <a href="#"  class = "blackButton" >Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="product-item">
+                <img src="product5.png" alt="play stand">
+                <div class="product-item-text">
+                    <h3>Play pole</h3>
+                    <div class="product-item-text-sub">
+                        <h3>$50</h3>
+                        <a href="#"  class = "blackButton" >Add to cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="product-item">
+                <img src="product4.png" alt="bowl">
+                <div class="product-item-text">
+                    <h3>Kitty litter 10kg</h3>
+                    <div class="product-item-text-sub">
+                        <h3>$10</h3>
+                        <a href="#"  class = "blackButton" >Add to cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="Locations">
+        <h1>Locations</h1>
+        <div class = "locationPlace">
+            <img src="location1.png" alt="Portland">
+            <div class = "locationPlaceText">
+                <h2>Portland</h2>
+                <p>Welcome to Catty Portland, part of a network of shelters dedicated to helping cats find their forever homes. Located in the heart of 
+                    Portland, our shelter is a safe haven for abandoned, stray, and neglected cats. At Catty Portland, we’re passionate about giving 
+                    every cat the care and attention they need to thrive while waiting for their new families. Whether you're looking for a playful 
+                    kitten or a calm adult cat, we have a variety of cats available for adoption, each with their unique personality.  Our adoption 
+                    process is straightforward, and our friendly staff is here to guide you in finding the perfect match. In addition to adoption, we 
+                    offer grooming services, pet supplies, and educational resources to help new cat owners. We believe in promoting responsible pet 
+                    ownership and are committed to ensuring every cat’s well-being.  </p>
+                <a href="#"  class = "whiteButton" >About</a>
+            </div>
+        </div>
+        <div class = "locationPlaceRight">
+            <div class = "locationPlaceTextRight" >
+                <h2>Portland</h2>
+                <p>Welcome to Catty Portland, part of a network of shelters dedicated to helping cats find their forever homes. Located in the heart of 
+                    Portland, our shelter is a safe haven for abandoned, stray, and neglected cats. At Catty Portland, we’re passionate about giving 
+                    every cat the care and attention they need to thrive while waiting for their new families. Whether you're looking for a playful 
+                    kitten or a calm adult cat, we have a variety of cats available for adoption, each with their unique personality.  Our adoption 
+                    process is straightforward, and our friendly staff is here to guide you in finding the perfect match. In addition to adoption, we 
+                    offer grooming services, pet supplies, and educational resources to help new cat owners. We believe in promoting responsible pet 
+                    ownership and are committed to ensuring every cat’s well-being.  </p>
+                <a href="#"  class = "whiteButton" >About</a>
+            </div>
+            <img src="location1.png" alt="Portland">
+        </div>
+        <div class = "locationPlace">
+            <img src="location1.png" alt="Portland">
+            <div class = "locationPlaceText">
+                <h2>Portland</h2>
+                <p>Welcome to Catty Portland, part of a network of shelters dedicated to helping cats find their forever homes. Located in the heart of 
+                    Portland, our shelter is a safe haven for abandoned, stray, and neglected cats. At Catty Portland, we’re passionate about giving 
+                    every cat the care and attention they need to thrive while waiting for their new families. Whether you're looking for a playful 
+                    kitten or a calm adult cat, we have a variety of cats available for adoption, each with their unique personality.  Our adoption 
+                    process is straightforward, and our friendly staff is here to guide you in finding the perfect match. In addition to adoption, we 
+                    offer grooming services, pet supplies, and educational resources to help new cat owners. We believe in promoting responsible pet 
+                    ownership and are committed to ensuring every cat’s well-being.  </p>
+                <a href="#"  class = "whiteButton" >About</a>
+            </div>
         </div>
     </section>
 
+    <footer>
+        catty
+    </footer>
 </section>
 </body>
 </html>
